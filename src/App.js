08983069066b15ae0d3d78581f83fc014fa3ad65
SantosRojas <santos777rojas@gmail.com>
2023-12-10 22:28:37 -0500

    change hash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './screens/Login';
 import AddMantos from './screens/AddMantos';
 import Home from './screens/Home';
